ef369c3ff4d7f30932ed64c9032f3047c43103f2
Max15-cyber <max80021151@gmail.com>
2026-01-14 00:16:51 +0300

    (ADD): Added update username route and function

--- auth/handlers/user_service.go
+++ auth/handlers/user_service.go
@@ -12,28 +12,44 @@ func UserService(w http.ResponseWriter, r *http.Request){
 	if r.URL.Query().Get("type") == "get_user_list" {
 		users, err := user_service.GetUserList()
 		if err != nil {
-		    http.Error(w, "no users in mongo :>", http.StatusBadRequest)
+		    http.Error(w, "no users in mongo :<", http.StatusBadRequest)
 		}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(users)
 	}
 
+
 	if r.URL.Query().Get("type") == "get_user_info" {
 		var req user_service.UserInfRequest
 
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			http.Error(w, "invalid request :>", http.StatusBadRequest)
+			http.Error(w, "invalid request :<", http.StatusBadRequest)
 			return
 		}
-		log.Println(req.UserId)
 
 		user, err := user_service.GetUserInfo(req.UserId)
 		if err != nil {
-		    http.Error(w, "no such user :>", http.StatusBadRequest)
+		    http.Error(w, "no such user :<", http.StatusBadRequest)
 			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(user)
+	}
+
+
+	if r.URL.Query().Get("type") == "update_full_name" {
+		var req user_service.UpdateUserNameRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "invalid request :<", http.StatusBadRequest)
+			return
+		}
+
+		if err := user_service.UpdateUserName(req.UserId, req.NewName); err != nil {
+			http.Error(w, "Couldn't update user :<", http.StatusNotFound)
+			log.Println("Couldn't update user :<", err)
+			return
+		}
+		w.Write([]byte("Username successfully updated!"))
 	}
 }
